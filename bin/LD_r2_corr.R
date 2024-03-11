20d54ef593229aa210e68f200a28a73327ec27e7
Maya Mastronardo <144179430+mmastro5@users.noreply.github.com>
2024-03-11 12:26:59 -0400

    reads in raw file, in large list

--- bin/LD_r2_corr.R
+++ bin/LD_r2_corr.R
@@ -7,23 +7,27 @@ library(ggplot2)
 library(data.table)
 library(optparse)
 
-
 #get date and time format as a variable YYYYMMDD_HHMM
 date <- format(Sys.time(), "%Y%m%d_%H%M")
 
 # Set up test data
-out_dir = glue::glue("analysis/{date}_test_marker_gene")
+out_dir = glue::glue("analysis/{date}_test_r2")
 
 params <- list( 
   
-  ce_LD_r2 = "projects/b1059/projects/Ryan/ortholog_sims/Caeno_Scan/ce.fullpop.ld.bin",
-  cb_LD_r2 = "projects/b1059/projects/Ryan/ortholog_sims/Caeno_Scan/ce.fullpop.ld.bin",
-  ct_LD_r2 = "projects/b1059/projects/Ryan/ortholog_sims/Caeno_Scan/ce.fullpop.ld.bin",
+  ce_LD_r2 = "Caeno_Scan/ce.fullpop.ld.bin",
+  cb_LD_r2 = "Caeno_Scan/cb.fullpop.ld.bin",
+  ct_LD_r2 = "Caeno_Scan/ct.fullpop.ld.bin",
   out_dir = out_dir 
 )
 
-# check if the directory exists, if not create it
-if (!dir.exists(params$out_dir)) {
-  dir.create(params$out_dir)
-}
-out_dir = params$out_dir
+# # check if the directory exists, if not create it
+# if (!dir.exists(params$out_dir)) {
+#   dir.create(params$out_dir)
+# }
+# out_dir = params$out_dir
+#
+# ct_file <- file(params$ct_LD_r2, "rb")
+# ct_LD_matrix <- readBin(ct_file, what="double")
+# 
+ct_file <- readr::read_lines_raw(params$ct_LD_r2)
