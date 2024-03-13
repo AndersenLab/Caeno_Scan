9794f17fba0efc6cf9d001e7fb129c87cdc4d968
mckeowr1 <mckeow.jr1@gmail.com>
2024-03-13 08:40:24 -0500

    script updates to handle all annotated SNPs input

--- bin/sim_og_effects.py
+++ bin/sim_og_effects.py
@@ -11,65 +11,78 @@ def load_strain_set_variants(strain_set_variant_file):
     Load the strain set variants from a plink .bim file
     ** THIS IS ALSO DEFINED IN simulate_orthogroup_vars.py **
     """
+
+    # TODO - add all columns 
     #Read in only the first 2 columns of the bim file with tab delimiter
-    strain_set_variants = pd.read_csv(strain_set_variant_file, sep="\t", header=None, usecols=[0,1], names=["CHROM", "ID"])
+    strain_set_variants = pd.read_csv(
+        strain_set_variant_file, 
+        sep="\t",
+        header=None, 
+        usecols=[3,5], 
+        names=["Marker", "Orthogroup"]
+        )
 
     return strain_set_variants
 
-def add_var_orthogroups(strain_set_variants, sp, passing_ogs, master_snps_dir):
-    """
-    ** THIS IS ALSO DEFINED IN simulate_orthogroup_vars.py **
+# def add_var_orthogroups(strain_set_variants, sp, passing_ogs, master_snps_dir):
+#     """
+#     ** THIS IS ALSO DEFINED IN simulate_orthogroup_vars.py **
 
-    Add the orthogroup information to the strain set variants
+#     Add the orthogroup information to the strain set variants
 
-    Parameters
-    ----------
-    strain_set_variants : pandas dataframe
-        The strain set variants with the following columns:
-        0: chromosome
-        1: variant id
-    sp : string
-        The species of the strain set variants
-        Reads in the master snps file with columns:
-        0: variant id (eg. 2:1000000)
-        1: transcript id (eg. C5641.2)
-        2: orthogroup id (eg. OG0000000)
-    """
-# load the master snps file for the species - the snp files are pulled from the defined master_snps_dir in the NF pipeline
-    if sp == "c_elegans":
-        master_snps_file = master_snps_dir + "/celeg-master.snps.genes"
-        print(master_snps_file)
-    elif sp == "c_briggsae":
-        master_snps_file = master_snps_dir + "/cbrig-master.snps.genes"
-    elif sp == "c_tropicalis":
-        master_snps_file = master_snps_dir + "/ctrop-master.snps.genes"
-    else:
-        print("Species not recognized")
-        return
-    #Read in the master snps file
-    master_snps = pd.read_csv(master_snps_file, header=None, names=['ID', "GENE", "ORTHOGROUP"], delimiter= ' ')
+#     Parameters
+#     ----------
+#     strain_set_variants : pandas dataframe
+#         The strain set variants with the following columns:
+#         0: chromosome
+#         1: variant id
+#     sp : string
+#         The species of the strain set variants
+#         Reads in the master snps file with columns:
+#         0: variant id (eg. 2:1000000)
+#         1: transcript id (eg. C5641.2)
+#         2: orthogroup id (eg. OG0000000)
+#     """
+# # load the master snps file for the species - the snp files are pulled from the defined master_snps_dir in the NF pipeline
+#     if sp == "c_elegans":
+#         master_snps_file = master_snps_dir + "/celeg-master.snps.genes"
+#         print(master_snps_file)
+#     elif sp == "c_briggsae":
+#         master_snps_file = master_snps_dir + "/cbrig-master.snps.genes"
+#     elif sp == "c_tropicalis":
+#         master_snps_file = master_snps_dir + "/ctrop-master.snps.genes"
+#     else:
+#         print("Species not recognized")
+#         return
+#     #Read in the master snps file
+#     master_snps = pd.read_csv(master_snps_file, header=None, names=['ID', "GENE", "ORTHOGROUP"], delimiter= ' ')
 
     
-    #Filter the master snps file to only include passing orthogroups
-    master_snps = master_snps[master_snps["ORTHOGROUP"].isin(passing_ogs)]
+#     #Filter the master snps file to only include passing orthogroups
+#     master_snps = master_snps[master_snps["ORTHOGROUP"].isin(passing_ogs)]
 
-    #Left join the master snps file to the strain set variants so that the orthogroup ids are added to the strain set variants using the variant id as the key
-    strain_set_variants = pd.merge(strain_set_variants, master_snps, how="left", left_on="ID", right_on="ID")
+#     #Left join the master snps file to the strain set variants so that the orthogroup ids are added to the strain set variants using the variant id as the key
+#     strain_set_variants = pd.merge(strain_set_variants, master_snps, how="left", left_on="ID", right_on="ID")
 
-    #remove the variants that do not have orthogroup ids
-    strain_set_variants = strain_set_variants.dropna()
-    #return the strain set variants with the orthogroup ids
-    return strain_set_variants
+#     #remove the variants that do not have orthogroup ids
+#     strain_set_variants = strain_set_variants.dropna()
+#     #return the strain set variants with the orthogroup ids
+#     return strain_set_variants
 
 
-def select_og_variants(og_variants, n_var_og = 1):
+def select_og_variants(og_variants, selected_ogs, n_var_og = 1):
     """
-    Group variants by orthogroup and select n_var_og variants from each orthogroup
+    Filter to variants in the selected orthogroups and select n_var_og variants from each orthogroup
     """
     #Group variants the data frame by orthogroup
-    og_variants = og_variants.groupby("ORTHOGROUP")
+    # TODO - maker sure the orthogroup column is named correctly in the new file format
+    sel_og_var = og_variants[og_variants["Orthogroup"].isin(selected_ogs)]
+
+    #Group the data frame by orthogroup
+    og_variants = sel_og_var.groupby("Orthogroup")
+    
     #Check the number of groups
-    print("Number of passing orthogroups: " + str(len(og_variants)))
+    #print("Number of passing orthogroups: " + str(len(og_variants)))
 
     #From each group select one variant
     og_variants = og_variants.apply(lambda x: x.sample(n_var_og)).reset_index(drop=True)
@@ -87,17 +100,22 @@ def simulate_og_effect_gamma(og_variants, n_var, og_effect_shape = 0.4, og_effec
 if __name__ == "__main__":
 
     #Define orthogroups from command line argument where the orthogroups are separated by commas
-    sim_ogs = sys.argv[1].split(",")
+    #sim_ogs = sys.argv[1].split(",")
+    sim_ogs = "OG0008648,OG0000678".split(",")
 
+    #get the number of orthogroups
+    n_ogs = len(sim_ogs)
 
     #Get the list of variants in the strain sets - from the .bim file
-    strain_set_variant_file = sys.argv[2]
+    #strain_set_variant_file = sys.argv[2]
+    strain_set_variant_file = "test_data/select_og_cv/ct.fullpop_0.00.bim.bed.annotated.gene_markers.tsv"
 
     #Get the directory of the master snps files
-    master_snps_dir = sys.argv[3]
+    #master_snps_dir = sys.argv[3]
 
     #Define the species
-    sp = sys.argv[4]
+    #sp = sys.argv[4]
+    sp = "c_tropicalis"
 
     #og1 = "OG0010644"
     #og1 = "OG0010836"
@@ -113,16 +131,16 @@ if __name__ == "__main__":
 
 
     #Add the passing orthogroup ids to the strain set variants and only return the variants that have orthogroup ids from the joining
-    print("Adding orthogroup ids to strain set variants")
-    og_vars = add_var_orthogroups(strain_var, sp, sim_ogs, master_snps_dir)
+    #print("Adding orthogroup ids to strain set variants")
+    #og_vars = add_var_orthogroups(strain_var, sp, sim_ogs, master_snps_dir)
     
     #Select Causal variants for orthogroups
-    causal_og_vars = select_og_variants(og_vars, 1)
+    causal_og_vars = select_og_variants(strain_var, sim_ogs, 1)
     print("Selected Causal Variants")
     print(causal_og_vars)
 
     #Simulate effects for causal variants
-    causal_og_vars = simulate_og_effect_gamma(causal_og_vars, 1)
+    causal_og_vars = simulate_og_effect_gamma(causal_og_vars, n_ogs)
     
     #Write output for trait simulations - just id and effect
-    causal_og_vars[["ID", "EFFECT"]].to_csv("causal_og_vars.txt", sep = " ", index = False, header = False)
+    causal_og_vars[["Marker", "EFFECT"]].to_csv("causal_og_vars.txt", sep = " ", index = False, header = False)
